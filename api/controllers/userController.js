47d41020fbec74962f7a9b9fca6ea668190357f9
TrieuLee <lehaitrieu48@gmail.com>
2022-12-23 00:43:37 +0700

    .

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -41,7 +41,7 @@ export const getUser = async (req, res, next) => {
 };
 export const getUsers = async (req, res, next) => {
   try {
-    console.log("hi");
+
     const users = await User.find();
     res.status(200).json(users);
   } catch (err) {

109d9eb9c8697d39091f58dc909fe5f3eedbf9d3
TrieuLee <lehaitrieu48@gmail.com>
2022-12-22 12:54:41 +0700

    .

--- api/models/Import.js
+++ api/models/Import.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 const ImportSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  warehousesId: {
-    type: String,
-    default: "",
-    required: true,
-  },
   materials: [
     {
       materialId: { type: String },
